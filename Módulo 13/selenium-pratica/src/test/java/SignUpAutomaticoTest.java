541db55cb117c772e1e4a9e70b9368cdf547c4d3
augustces <augustces@alu.ufc.br>
2024-06-12 20:29:08 -0300

    Modulo 13 - Correção

--- Módulo 13/selenium-pratica/src/test/java/SignUpAutomaticoTest.java
+++ Módulo 13/selenium-pratica/src/test/java/SignUpAutomaticoTest.java
@@ -1,43 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import com.example.User;
 
 public class SignUpAutomaticoTest {
     private WebDriver navegador;
-    private List<User> users = new ArrayList<>();
+    private Random rand = new Random();
+    private int value = rand.nextInt(10000)+1;
 
-    @Test
-    public void automacao(){
-        
-        // Lista de usuários para teste
-        users.add(new User("usuario1@example.com", "Usuario um"));
-        users.add(new User("usuario2@example.com", "Usuario dois"));
-        users.add(new User("usuario3@example.com", "Usuario três"));
-
-        for (User user : users) {
+    @BeforeTest
+    public void setUp(){
         // Cria instância para acessar o navegador no site informado
         navegador = new ChromeDriver();
         navegador.get("https://automationexercise.com/login");
+    }
+
+    @Test
+    public void automacao(){
         // Selecionar formulário
         WebElement signUpForm = navegador.findElement(By.className("signup-form"));
 
         // Preencher campo Name
-        signUpForm.findElement(By.name("name")).sendKeys(user.getName());
+        signUpForm.findElement(By.name("name")).sendKeys("CursoQA");
 
         // Preencher campo e-mail
-        signUpForm.findElement(By.name("email")).sendKeys(user.getEmail());
+        signUpForm.findElement(By.name("email")).sendKeys("usuario" + value + "@example.com");
 
         // Clicar no botão Sign Up
         navegador.findElement(By.xpath("/html/body/section/div/div/div[3]/div/form/button")).click();
 
+    }
+
+    @AfterTest
+    public void tearDown(){
         navegador.quit();
-        }
     }
 }
